7581a3783fe67e6308adef8f8bcae56e8c24be23
Mayuri2306 <mayurijhade104@gmail.com>
2026-05-17 00:16:46 +0530

    added path to server.js

--- server.js
+++ server.js
@@ -51,3 +51,8 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("Backend Running");
+});
